305f97b0a17239c0083aaf9529327c420b6c2008
namem <namem_br@hotmail.com>
2025-04-22 16:14:27 -0400

    teste

---
+++ int_dec.py
@@ -0,0 +1,18 @@
+
+while(True):
+    numero = input("Entre com numero")
+    try:
+        if(numero == int or float):
+            print("valido")
+        
+    except:
+        print('invalido')
+        break
+    
+
+
+###terminar
+
+
+
+
---
+++ sequencia.py
@@ -0,0 +1,16 @@
+def se_valido(s):
+    if not s:
+        return True
+    if (len(s) %2==0):
+        return True
+    if (len(s)//2=='0') * (len(s)//=='1') and :
+        return True
+
+
+entrada= input("entre com os numeros")
+
+if (se_valido(entrada)=True):
+    print('valido')
+
+else:
+    print("Numero invalido")
